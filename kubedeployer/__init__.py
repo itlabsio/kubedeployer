aae7352d7597a42bb2cfeb48a1f368641f454444
asagitullin <asagitullin@sdvor.com>
2023-06-14 12:45:42 +0500

    add args to run local

--- kubedeployer/__init__.py
+++ kubedeployer/__init__.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 from typing import Type
 
 from kubedeployer import deploy
@@ -8,6 +9,8 @@ from kubedeployer.deployer.orthodox_deployer import OrthodoxDeployer
 from kubedeployer.deployer.smart_deployer import SmartDeployer
 
 __all__ = ['run_kubedeployer']
+
+from kubedeployer.gitlab_ci import specification
 
 
 def run_kubedeployer():
@@ -32,12 +35,31 @@ def run_kubedeployer():
         help="read in a file of environment variables",
     )
 
+    parser.add_argument(
+        "--project-dir",
+        type=str,
+        help="full path to folder with project",
+    )
+
+    parser.add_argument(
+        "--manifest-folder",
+        type=str,
+        help="path to folder with manifest, path is relative to current working directory",
+    )
+
     args = parser.parse_args()
     deployer_type = args.deployer
+    dry_run = args.dry_run
+    if args.manifest_folder:
+        os.environ[specification.MANIFEST_FOLDER_ENV_VAR] = args.manifest_folder
+    if args.project_dir:
+        os.environ[specification.CI_PROJECT_DIR_ENV_VAR] = args.project_dir
+    if dry_run:
+        os.environ[specification.CI_PROJECT_ID_ENV_VAR] = '0'
     deployer_classes = {
         'orthodox': OrthodoxDeployer,
         'kustomize': KustomizeDeployer,
         'smart': SmartDeployer
     }
     deployer: Type[AbstractDeployer] = deployer_classes[deployer_type]
-    deploy.run(deployer=deployer, dry_run=args.dry_run, env_files=args.env_file)
+    deploy.run(deployer=deployer, dry_run=dry_run, env_files=args.env_file)
