886915f03b182a80adedd02b2b86b3efd1ef444d
Vladimir Samsonov <vnsamsonov@gmail.com>
2023-12-13 00:35:36 +0500

    add cleanup for tests and remove deprecating `--record` option for kubectl

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,8 +1,61 @@
 #!/bin/sh
 
-docker stop deployer_runner
-docker rm deployer_runner
+need_cleanup=false
 
-docker build --progress=plain -t deployer -f tests/containers/Dockerfile .
-chmod +x tests/containers/integration_tests/integration_entrypoint.sh
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock --add-host=host.docker.internal:host-gateway --entrypoint=tests/containers/integration_tests/integration_entrypoint.sh --name=deployer_runner deployer:latest
+cleanup() {
+  if [ "$need_cleanup" = false ]; then
+    return
+  fi
+
+  container_output_fmt="{{.ID}} ({{.Image}})"
+
+  docker ps --format "$container_output_fmt" | while IFS= read -r container_info; do
+    container_id=$(echo "$container_info" | cut -d ' ' -f 1)
+    docker stop "$container_id" > /dev/null \
+      && echo "Stopping container $container_info"
+  done
+
+  docker ps -a --format "$container_output_fmt" | while IFS= read -r container_info; do
+    container_id=$(echo "$container_info" | cut -d ' ' -f 1)
+    docker rm "$container_id" > /dev/null \
+      && echo "Removing container $container_info"
+  done
+}
+
+startup() {
+  while [ "$#" -gt 0 ]
+  do
+    case "$1" in
+      --rm)
+        need_cleanup=true
+        shift
+        ;;
+      --help|*)
+        echo "Run integration tests locally. Usage:"
+        echo "  --rm"
+        echo "    Automatically stop and remove all containers when tests done"
+        echo "  --help"
+        echo "    Print usage"
+        exit 1
+        ;;
+    esac
+  done
+
+  # Stop and remove running containers for escape conflicts
+  cleanup
+}
+
+run() {
+  docker build --progress=plain -t deployer -f tests/containers/Dockerfile .
+  chmod +x tests/containers/integration_tests/integration_entrypoint.sh
+  docker run --rm \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    --add-host=host.docker.internal:host-gateway \
+    --entrypoint=tests/containers/integration_tests/integration_entrypoint.sh \
+    --name=deployer_runner deployer:latest
+}
+
+
+startup "$@"
+run
+cleanup
